c2d0e370ac109cf3ca204ed4c05d1981b87cc312
Angel9098 <90285177+Angel9098@users.noreply.github.com>
2023-07-25 11:25:10 -0600

    Inicio

---
+++ Sistema/src/test/java/SIC/SistemasContables/ContableApplicationTests.java
@@ -0,0 +1,13 @@
+package SIC.SistemasContables;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
